02a5795316f45ae5c2bf96cd41b570aa88ec9d9e
Jaya Aditya Vignesh Illa <155290978+Jaya-Aditya-Vignesh@users.noreply.github.com>
2023-12-31 19:51:46 +0530

    Add files via upload

---
+++ Aditya.java
@@ -0,0 +1,18 @@
+package aditya;
+
+/**
+ *
+ * @author Jaya Aditya Vignesh
+ */
+public class Aditya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+new Login_page().setVisible(true);
+    }
+    
+}
